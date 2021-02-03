9613705a8487f5f94b9eb0641b0bab063ab50ab0
mir-am <mir-am@hotmail.com>
2021-02-03 17:12:06 +0100

    Commented out a legacy function

--- libsa4py/utils.py
+++ libsa4py/utils.py
@@ -40,14 +40,15 @@ def ParallelExecutor(use_bar='tqdm', **joblib_args):
     return aprun
 
 
-def filter_directory(directory: str, extension: str = '.py') -> str:
-    """
-    Delete all files within the given directory with filenames not ending in the given extension
-    """
-    for root, dirs, files in os.walk(directory):
-        [os.remove(os.path.join(root, fi)) for fi in files if not fi.endswith(extension)]
-
-    return directory
+# This is a legacy DESTRUCTIVE function that should be removed
+# def filter_directory(directory: str, extension: str = '.py') -> str:
+#     """
+#     Delete all files within the given directory with filenames not ending in the given extension
+#     """
+#     for root, dirs, files in os.walk(directory):
+#         [os.remove(os.path.join(root, fi)) for fi in files if not fi.endswith(extension)]
+#
+#     return directory
 
 
 def list_files(directory: str) -> list:
