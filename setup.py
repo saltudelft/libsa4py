ccebc7b0f3f1701cb3c931917692846deb888aa0
mir-am <mir-am@hotmail.com>
2021-03-04 14:46:30 +0100

    Update .gitignore and setup.py files

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Operating System :: Unix',
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
