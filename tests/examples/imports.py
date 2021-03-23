9bc522e0200a2802eb98640a290f00789dbc193d
mir-am <mir-am@hotmail.com>
2021-03-23 15:23:09 +0100

    Add units tests for imports example

--- tests/examples/imports.py
+++ tests/examples/imports.py
@@ -6,3 +6,8 @@ import math
 import typing as t
 from os.path import join, split
 from string import *
+
+
+def inner_imports():
+    import collections
+    collections.Counter()
---
+++ tests/test_imports.py
@@ -0,0 +1,30 @@
+from libsa4py.cst_extractor import Extractor
+import unittest
+
+
+class TestImports(unittest.TestCase):
+    """
+    It tests various forms of imports in a file
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def setUp(self):
+        self.processed_f = Extractor().extract(open('./examples/imports.py', 'r').read())
+
+    def test_import_single_module(self):
+        print(self.processed_f['imports'])
+        imp_single_mod_exp = 'math'
+        self.assertEqual(imp_single_mod_exp in self.processed_f['imports'], True)
+
+    def test_import_alias(self):
+        imp_alias_exp = 'typing'
+        self.assertEqual(imp_alias_exp in self.processed_f['imports'], True)
+
+    def test_inner_imports(self):
+        imp_inner_imports = 'collections'
+        self.assertEqual(imp_inner_imports in self.processed_f['imports'], True)
+
+    def test_star_import(self):
+        pass  # TODO: extracting star imports?
