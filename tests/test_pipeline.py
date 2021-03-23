d23a8682afe80b29a74f6691685ea174f4658e93
mir-am <mir-am@hotmail.com>
2021-03-23 19:42:46 +0100

    Add unit tests for testing the output of LibSA4Py's pipeline

---
+++ tests/test_pipeline.py
@@ -0,0 +1,31 @@
+from libsa4py.cst_pipeline import Pipeline
+from pathlib import Path
+from os.path import join
+import unittest
+import json
+import shutil
+
+
+class TestPipeline(unittest.TestCase):
+    """
+    It tests the pipeline of LibSA4Py
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @classmethod
+    def setUpClass(cls):
+        p = Pipeline(Path(__file__).parent.absolute().parent,
+                     join(Path(__file__).parent.absolute(), 'tmp'))
+        p.run([{'author': 'tests', 'repo': 'examples'}], 1)
+
+    def test_pipeline_output(self):
+        pipeline_out_exp = json.loads(open("./examples/testsexamples.json", 'r').read())
+        pipeline_out = json.loads(open("./tmp/processed_projects/testsexamples.json", 'r').read())
+
+        self.assertDictEqual(pipeline_out_exp, pipeline_out)
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree("./tmp/")
