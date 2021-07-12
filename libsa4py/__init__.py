abf6209c6acd64bc1b0b759ce677e95abc2c04e2
mir-am <mir-am@hotmail.com>
2021-07-12 14:03:21 +0200

    Add Python builtins types to the package constants

--- libsa4py/__init__.py
+++ libsa4py/__init__.py
@@ -9,19 +9,6 @@ UNK_TYPE_ANNOT = 'U'
 MAX_TC_TIME = 120
 
 # Python types
-PY_TYPING_MOD = {'AbstractSet', 'Any', 'AnyStr', 'AsyncContextManager', 'AsyncGenerator', 'AsyncIterable',
-                 'AsyncIterator', 'Awaitable', 'BinaryIO', 'ByteString', 'CT_co', 'Callable', 'ChainMap', 'ClassVar',
-                 'Collection', 'Container', 'ContextManager', 'Coroutine', 'Counter', 'DefaultDict', 'Deque', 'Dict',
-                 'ForwardRef', 'FrozenSet', 'Generator', 'Generic', 'Hashable', 'IO', 'ItemsView', 'Iterable',
-                 'Iterator', 'KT', 'KeysView', 'List', 'Literal', 'Mapping', 'MappingView', 'Match',
-                 'MethodDescriptorType', 'MethodWrapperType', 'MutableMapping', 'MutableSequence', 'MutableSet',
-                 'NamedTuple', 'NamedTupleMeta','NewType', 'NoReturn', 'Optional', 'OrderedDict', 'Pattern',
-                 'Reversible', 'Sequence', 'Set', 'Sized','SupportsAbs', 'SupportsBytes', 'SupportsComplex',
-                 'SupportsFloat', 'SupportsInt', 'SupportsRound', 'TYPE_CHECKING', 'T_co', 'T_contra', 'Text', 'TextIO',
-                 'Tuple', 'Type', 'TypeVar', 'Union', 'VT', 'VT_co', 'V_co', 'ValuesView', 'WrapperDescriptorType',
-                 '_Final', '_GenericAlias', '_Immutable', '_Protocol', '_ProtocolMeta', '_SpecialForm', '_TypingEllipsis', '_TypingEmpty',
-                 '_VariadicGenericAlias'}
-
 PY_TYPING_MOD = {'ABCMeta', 'AbstractSet', 'Any', 'AnyStr', 'AsyncContextManager', 'AsyncGenerator', 'AsyncIterable',
                  'AsyncIterator', 'Awaitable', 'BinaryIO', 'ByteString', 'CT_co', 'Callable', 'ChainMap', 'ClassVar',
                  'Collection', 'Container', 'ContextManager', 'Coroutine', 'Counter', 'DefaultDict', 'Deque', 'Dict',
@@ -39,3 +26,25 @@ PY_TYPING_MOD = {'ABCMeta', 'AbstractSet', 'Any', 'AnyStr', 'AsyncContextManager
 PY_COLLECTION_MOD = {'ChainMap', 'Counter', 'OrderedDict', 'UserDict', 'UserList', 'UserString', '_Link',
                      '_OrderedDictItemsView', '_OrderedDictKeysView', '_OrderedDictValuesView', 'defaultdict',
                      'deque', 'namedtuple'} - PY_TYPING_MOD
+
+PY_BUILTINS_MOD = {'ArithmeticError', 'AssertionError', 'AttributeError', 'BaseException', 'BlockingIOError',
+                   'BrokenPipeError', 'BufferError', 'BytesWarning', 'ChildProcessError', 'ConnectionAbortedError',
+                   'ConnectionError', 'ConnectionRefusedError', 'ConnectionResetError', 'DeprecationWarning',
+                   'EOFError', 'Ellipsis', 'EnvironmentError', 'Exception', 'False', 'FileExistsError',
+                   'FileNotFoundError', 'FloatingPointError', 'FutureWarning', 'GeneratorExit', 'IOError',
+                   'ImportError', 'ImportWarning', 'IndentationError', 'IndexError', 'InterruptedError',
+                   'IsADirectoryError', 'KeyError', 'KeyboardInterrupt', 'LookupError', 'MemoryError',
+                   'ModuleNotFoundError', 'NameError', 'None', 'NotADirectoryError', 'NotImplemented',
+                   'NotImplementedError', 'OSError', 'OverflowError', 'PendingDeprecationWarning', 'PermissionError',
+                   'ProcessLookupError', 'RecursionError', 'ReferenceError', 'ResourceWarning', 'RuntimeError',
+                   'RuntimeWarning', 'StopAsyncIteration', 'StopIteration', 'SyntaxError', 'SyntaxWarning',
+                   'SystemError', 'SystemExit', 'TabError', 'TimeoutError', 'True', 'TypeError', 'UnboundLocalError',
+                   'UnicodeDecodeError', 'UnicodeEncodeError', 'UnicodeError', 'UnicodeTranslateError',
+                   'UnicodeWarning', 'UserWarning', 'ValueError', 'Warning', 'ZeroDivisionError',
+                   'abs', 'all', 'any', 'ascii', 'bin', 'bool', 'breakpoint', 'bytearray', 'bytes', 'callable', 'chr',
+                   'classmethod', 'compile', 'complex', 'copyright', 'credits', 'delattr', 'dict', 'dir', 'divmod',
+                   'enumerate', 'eval', 'exec', 'exit', 'filter', 'float', 'format', 'frozenset', 'getattr', 'globals',
+                   'hasattr', 'hash', 'help', 'hex', 'id', 'input', 'int', 'isinstance', 'issubclass', 'iter', 'len',
+                   'license', 'list', 'locals', 'map', 'max', 'memoryview', 'min', 'next', 'object', 'oct', 'open',
+                   'ord', 'pow', 'print', 'property', 'quit', 'range', 'repr', 'reversed', 'round', 'set', 'setattr',
+                   'slice', 'sorted', 'staticmethod', 'str', 'sum', 'super', 'tuple', 'type', 'vars', 'zip'}
