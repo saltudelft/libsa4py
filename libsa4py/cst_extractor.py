db2fab50a83656dabacf0ad7bb4e2985a1b62756
mir-am <mir-am@hotmail.com>
2021-07-14 10:57:47 +0200

    Make Seq2Seq representation optional in Extractor

--- libsa4py/cst_extractor.py
+++ libsa4py/cst_extractor.py
@@ -15,7 +15,8 @@ class Extractor:
 
     @staticmethod
     def extract(program: str,
-                program_types: cst.metadata.type_inference_provider.PyreData = None) -> ModuleInfo:
+                program_types: cst.metadata.type_inference_provider.PyreData = None,
+                include_seq2seq: bool = True) -> ModuleInfo:
         try:
             parsed_program = cst.parse_module(program)
         except Exception as e:
@@ -33,26 +34,30 @@ class Extractor:
             mw = cst.metadata.MetadataWrapper(program_tqr, cache={cst.metadata.TypeInferenceProvider: {'types':[]}})
             mw.visit(v)
 
-        # Transformers
-        v_cm_doc = CommentAndDocStringRemover()
-        v_str = StringRemover()
-        v_num = NumberRemover()
-        v_type = TypeAnnotationRemover()
-        v_type_add = TypeAdder(v.module_all_annotations)
-        v_space = SpaceAdder()
+        if include_seq2seq:
+            # Transformers
+            v_cm_doc = CommentAndDocStringRemover()
+            v_str = StringRemover()
+            v_num = NumberRemover()
+            v_type = TypeAnnotationRemover()
+            v_type_add = TypeAdder(v.module_all_annotations)
+            v_space = SpaceAdder()
 
-        v_untyped = parsed_program.visit(v_cm_doc)
-        v_untyped = v_untyped.visit(v_str)
-        v_untyped = v_untyped.visit(v_num)
-        v_untyped = v_untyped.visit(v_type)
+            v_untyped = parsed_program.visit(v_cm_doc)
+            v_untyped = v_untyped.visit(v_str)
+            v_untyped = v_untyped.visit(v_num)
+            v_untyped = v_untyped.visit(v_type)
 
-        # Replaces identifiers with their type annotations
-        v_typed = v_untyped.visit(v_type_add)
+            # Replaces identifiers with their type annotations
+            v_typed = v_untyped.visit(v_type_add)
 
-        # Adding space for better tokenization
-        v_untyped = v_untyped.visit(v_space)
-        v_typed = v_typed.visit(v_space)
+            # Adding space for better tokenization
+            v_untyped = v_untyped.visit(v_space)
+            v_typed = v_typed.visit(v_space)
 
-        return ModuleInfo(v.imports, v.module_variables, v.module_variables_use, v.module_vars_ln, v.cls_list, v.fns,
-                          normalize_module_code(v_untyped.code), create_output_seq(normalize_module_code(v_typed.code)),
-                          v.module_no_types, v.module_type_annot_cove)
+            return ModuleInfo(v.imports, v.module_variables, v.module_variables_use, v.module_vars_ln, v.cls_list, v.fns,
+                              normalize_module_code(v_untyped.code), create_output_seq(normalize_module_code(v_typed.code)),
+                              v.module_no_types, v.module_type_annot_cove)
+        else:
+            return ModuleInfo(v.imports, v.module_variables, v.module_variables_use, v.module_vars_ln, v.cls_list,
+                              v.fns, "", "", v.module_no_types, v.module_type_annot_cove)
